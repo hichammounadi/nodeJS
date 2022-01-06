45bac4092c910ac0480dcf704fb538c81e60295b
hichammounadi <hicham.hamza.mounadi@gmail.com>
2022-01-06 15:32:16 +0100

    Event Emitter basics

---
+++ 15-EventEmitter.js
@@ -0,0 +1,10 @@
+const EventEmitter = require('events');
+
+//instance 
+const customEmitter = new EventEmitter()
+
+customEmitter.on('response', () => {
+  console.log(`data received`);
+})
+
+customEmitter.emit('response')
