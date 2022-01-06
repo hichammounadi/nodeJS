0920de6d097e49e1e44636a61dcbf054fa29215d
hichammounadi <hicham.hamza.mounadi@gmail.com>
2022-01-06 09:50:19 +0100

    node module basics

--- app.js
+++ app.js
@@ -3,7 +3,11 @@
 
 const names = require('./4-names')
 const sayHi = require('./5-utils')
+const data = require ('./6-alternative-flavor')
+require('./7-mind-grenade')
 
-sayHi('susan')
-sayHi(names.john)
-sayHi(names.peter)
+// console.log(data)
+// sayHi('susan')
+
+// sayHi(names.john)
+// sayHi(names.peter)
